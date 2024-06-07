e345848055999059f56497dff39e505afc85efb2
Shivam-Singh0 <110924353+Shivam-Singh0@users.noreply.github.com>
2024-06-07 22:55:06 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,18 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors())
 
-app.use((req, res, next) => {
-    const domain = '1.1.1.1'; // Replace with your domain
-    
-    dns.resolve4(domain, (err, addresses) => {
-      if (err) {
-        console.error(`DNS lookup failed for ${domain}:`, err);
-      } else {
-        console.log(`Addresses for ${domain}:`, addresses);
-      }
-      next();
-    });
-  });
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/movies", movieRoutes)
