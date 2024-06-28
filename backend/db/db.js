83a385cec67a1df2e45725e32c98089a2c7ed615
Shivam-Singh0 <110924353+Shivam-Singh0@users.noreply.github.com>
2024-06-29 00:53:32 +0530

    env

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 
 export default async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const mongoURI = process.env.MONGO_URI;
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log(error);
