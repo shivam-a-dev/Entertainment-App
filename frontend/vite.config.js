94c4c1f0062b6e8d643af96e34dba490599cb9d4
Shivam-Singh0 <110924353+Shivam-Singh0@users.noreply.github.com>
2024-06-29 20:51:15 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://entertainment-app-live.onrender.com',
+      '/api': 'https://localhost:3000',
     }
   },
 })
