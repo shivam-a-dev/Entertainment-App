dd7d527f1425d0839e75449647a1105f4144c92f
Shivam-Singh0 <110924353+Shivam-Singh0@users.noreply.github.com>
2024-06-29 20:58:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Movies/Home';
 import Login from './pages/Auth/Login';
 import PrivateRoute from './pages/Auth/ProtectedRoutes';
